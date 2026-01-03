b4ab9f81709095f94be5e373cf8faebfda3b44b3
Nayomi Pavithra <nayomipavithra837@gmail.com>
2026-01-03 06:56:08 +0530

    Add files via upload

---
+++ LED_Design_01/LED_Design_01.ino
@@ -0,0 +1,76 @@
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(2,HIGH);
+  digitalWrite(8,HIGH);
+    delay(250);
+ 
+  digitalWrite(2,LOW);
+  digitalWrite(8,LOW);
+   delay(250);
+
+  digitalWrite(3,HIGH);
+  digitalWrite(7,HIGH);
+    delay(250);
+ 
+  digitalWrite(3,LOW);
+  digitalWrite(7,LOW);
+  delay(250);
+
+  digitalWrite(4,HIGH);
+  digitalWrite(6,HIGH);
+    delay(250);
+ 
+  digitalWrite(4,LOW);
+  digitalWrite(6,LOW);
+    delay(250);
+
+  digitalWrite(5,HIGH);
+  delay(250);
+  digitalWrite(5,LOW);
+
+digitalWrite(4,HIGH);
+  digitalWrite(6,HIGH);
+    delay(250);
+ 
+  digitalWrite(4,LOW);
+  digitalWrite(6,LOW);
+    delay(250);
+
+    digitalWrite(3,HIGH);
+  digitalWrite(7,HIGH);
+    delay(250);
+ 
+  digitalWrite(3,LOW);
+  digitalWrite(7,LOW);
+  delay(250);
+
+digitalWrite(2,HIGH);
+  digitalWrite(8,HIGH);
+    delay(250);
+ 
+  digitalWrite(2,LOW);
+  digitalWrite(8,LOW);
+   delay(250);
+
+
+
+
+
+
+
+
+
+
+}
+
